11dce148af29a3423712e8a024952eb1b82f52ed
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-03-12 23:09:20 -0500

    clock changes

--- clock.js
+++ clock.js
@@ -2,4 +2,18 @@ setInterval(showTime, 1000);
 function showTime() {
     let time = new Date();
     let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    am_pm = "AM";
+
+    if (hour > 12) {
+        hour -= 12;
+        am_pm = "PM";
+    }
+    if (hour == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
 }
